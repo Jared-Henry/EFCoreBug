db59e90aea18b305bdc2dbd755ceaa3e0c2b1f96
Jared Henry <jared.klayton.henry@gmail.com>
2017-05-08 18:28:25 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            try
+            {
+                var builder = new DbContextOptionsBuilder<TestDbContext>();
+                builder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Integrated Security=true;Database=Test");
+                using (var db = new TestDbContext(builder.Options))
+                {
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                    db.Things.Add(new Thing("Thing1", new User("User1")));
+                    db.SaveChanges();
+                }
+                using (var db = new TestDbContext(builder.Options))
+                {
+                    //This throws an exception because the "Thing" object
+                    //is instantiated during the request. The constructor
+                    //of the Thing class should not be called when the query 
+                    //is materialized to an anonymous type.
+                    var result = (from t in db.Things
+                                  select new
+                                  {
+                                      t.Id,
+                                      LastModifiedBy = t.LastModifiedBy.Username
+                                  }).ToArray();
+                }
+            }
+            catch(ConstructorCalledException ex)
+            {
+                Console.WriteLine("Error: Constructor was called during request.");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class ConstructorCalledException : Exception
+    {
+    }
+
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+        public DbSet<Thing> Things { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class Thing
+    {
+        public Thing()
+        {
+            throw new ConstructorCalledException();
+        }
+        public Thing(string name, User lastModifiedBy)
+        {
+            this.Name = name;
+            this.LastModifiedBy = lastModifiedBy;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User LastModifiedBy { get; set; }
+    }
+
+    public class User
+    {
+        public User()
+        {
+            throw new ConstructorCalledException();
+        }
+        public User(string username)
+        {
+            this.Username = username;
+        }
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
